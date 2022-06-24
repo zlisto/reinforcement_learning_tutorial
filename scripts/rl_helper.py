0528d884c7d6b92e23f0063206a192c475e0ea8d
Tauhid Zaman <zlisto@gmail.com>
2022-06-23 21:42:36 -0400

    scripts

---
+++ scripts/rl_helper.py
@@ -0,0 +1,50 @@
+import gym
+from gym.wrappers import Monitor
+import glob
+import io
+import base64
+from IPython.display import HTML
+from pyvirtualdisplay import Display
+from IPython import display as ipythondisplay
+
+
+
+
+
+
+def query_environment(name):
+    env = gym.make(name)
+    spec = gym.spec(name)
+    print(f"Action Space: {env.action_space}")
+    print(f"Observation Space: {env.observation_space}")
+    print(f"Max Episode Steps: {spec.max_episode_steps}")
+    print(f"Nondeterministic: {spec.nondeterministic}")
+    print(f"Reward Range: {env.reward_range}")
+    print(f"Reward Threshold: {spec.reward_threshold}")
+
+def show_video():
+    mp4list = glob.glob('video/*.mp4')
+    if len(mp4list) > 0:
+        mp4 = mp4list[0]
+        video = io.open(mp4, 'r+b').read()
+        encoded = base64.b64encode(video)
+        ipythondisplay.display(HTML(data='''<video alt="test" autoplay 
+                controls style="height: 500px;">
+                <source src="data:video/mp4;base64,{0}" type="video/mp4" />
+             </video>'''.format(encoded.decode('ascii'))))
+    else:
+        print("Could not find video")
+
+
+def wrap_env(env):
+    env = Monitor(env, './video', force=True)
+    return env
+
+def calc_qvals(rewards):
+    res = []
+    sum_r = 0.0
+    for r in reversed(rewards):
+        sum_r *= GAMMA
+        sum_r += r
+        res.append(sum_r)
+    return list(reversed(res))
